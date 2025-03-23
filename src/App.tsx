6d4be1e81c063deade408285c72ca8ecb2eb7fd8
L145 <147063982+legelff@users.noreply.github.com>
2025-03-23 15:09:13 +0100

    reset defaultt styling and add custom base styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className='container'>
+        <h1 className='header'>
+          Hello world
+        </h1>
+
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo hic temporibus obcaecati dolores animi omnis saepe, porro asperiores nulla ut sapiente perspiciatis tenetur debitis est odit similique tempora at nemo.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
