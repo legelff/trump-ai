e5e3bddc57a36e3d88d816b0cf8865a43c87409c
L145 <147063982+legelff@users.noreply.github.com>
2025-03-24 23:26:52 +0100

    remove env...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,64 @@
+import { useRef, useState, useEffect } from 'react';
 import './App.css'
 import trumpMain from './assets/img/trumpmain.jpg';
+import axios from 'axios';
 
 import TrumpMsg from './components/TrumpMsg/TrumpMsg';
 import UserMsg from './components/UserMsg/UserMsg';
 
+
+interface Message {
+  role: string,
+  message: string | undefined;
+}
+
 function App() {
+  const refVal = useRef<HTMLTextAreaElement>(null);
+  const [msgs, setMsgs] = useState<Message[]>([]);
+
+  const handleClick = () => {
+    sendMessage();
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      if (!e.shiftKey) {
+        e.preventDefault(); 
+
+        // only enter is pressed -> send message through api
+        sendMessage();
+      }
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(msgs);
+  // }, [msgs]);
+
+  const client = axios.create({
+    baseURL: "http://localhost:3000"
+  })
+
+  const sendMessage = async () => {
+    // console.log(refVal.current?.value)
+    if (refVal.current && refVal.current.value !== "") {
+      const msg = refVal.current?.value;
+
+      const newUserMessage = { role: 'user', message: msg };
+      setMsgs((prevMsgs) => [...prevMsgs, newUserMessage]);
+
+      // communicate with api
+      const response = await client.post('/chat', {
+        message: msg,
+      });
+
+      const newTrumpMessage = { role: 'assistant', message: response.data.response };
+      setMsgs((prevMsgs) => [...prevMsgs, newTrumpMessage]);
+
+      refVal.current.value = '';
+    }
+  }
+
   return (
     <>
     <div className='container'>
@@ -19,19 +73,27 @@ function App() {
           </h1>
 
           <div className='txt'>
-            <TrumpMsg/>
-
-            <UserMsg/>
-
-            <TrumpMsg/>
-
-            <UserMsg/>
+            {msgs.map((item, index) => {
+              return (
+                <div key={index}>
+                  {item.role === "user" ? (
+                    <UserMsg message={item.message}/>
+                  ) : (
+                    <TrumpMsg message={item.message}/>
+                  )}
+                </div>
+              )
+            })}
           </div>
         </div>
 
         <div className='input-container'>
-          <textarea name="inputField" id="inputField" placeholder="I'm intelligent. Some people would say I'm very, very, very intelligent."></textarea>
-          <button className='submit'>
+          <textarea name="inputField" 
+                    id="inputField" 
+                    ref={refVal} 
+                    placeholder="I'm intelligent. Some people would say I'm very, very, very intelligent."
+                    onKeyDown={handleKeyDown}></textarea>
+          <button className='submit' onClick={handleClick}>
             <i className='bx bx-up-arrow-alt'></i>
           </button>
         </div>
--- frontend/src/components/UserMsg/UserMsg.tsx
+++ frontend/src/components/UserMsg/UserMsg.tsx
@@ -1,18 +1,23 @@
+import React from 'react';
 import './UserMsg.css';
 
-function UserMsg() {
+interface Props {
+  message: string | undefined;
+}
+
+const UserMsg: React.FC<Props> = ({message}) => {
     return (
         <div className='user-item-container'>
                 <div className='user-pfp-name-container'>
                   <div className='user-name-container'>
-                    User
+                    You
                   </div>
 
                   <div className='user-pfp-container'></div>
                 </div>
 
                 <div className='user-txt-container'>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, maxime provident itaque voluptates dolorem omnis quas commodi unde quasi, ab sed, eius quisquam mollitia nobis. Consequuntur totam eum placeat ab.</p>
+                  <p>{message}</p>
                 </div>
         </div>
     )
